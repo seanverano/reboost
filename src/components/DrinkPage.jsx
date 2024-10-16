4423ea535d5c354c2b9348b861c8523431065155
Sean Dustin Verano <verano.seandustin@gmail.com>
2024-10-17 02:48:25 +0800

    Add button to DrinkPage and install react-icon lib

--- src/components/DrinkPage.jsx
+++ src/components/DrinkPage.jsx
@@ -1,10 +1,34 @@
 import React from "react";
+import { FaPlus } from "react-icons/fa6";
 
 const DrinkPage = () => {
   return (
     <>
-      <div className="flex justify-center items-center h-full">
-        <h1 className="font-noto text-4xl">Hydrate Daily!</h1>
+      <div className="flex flex-col h-full w-[300px] h-[500px]">
+        <h1 className="font-noto text-lg justify-center text-center">
+          Hydrate Daily!
+        </h1>
+        <h2 className="font-noto text-sm text-left p-0 m-0">Today</h2>
+        <div className="flex px-4 py-6 justify-center items-center">
+          <div className="flex flex-1 flex-col gap-2 rounded-xl border border-transparent p-6 bg-[#EEF8FB]">
+            <p className="text-[#000000]">0% Hydrated</p>
+            <p className="text-[#000000] font-bold">40oz</p>
+          </div>
+        </div>
+      </div>
+      <div>
+        <button
+          className="text-sm font-jakarta font-bold
+    text-[#ffffff] bg-[#55BCD8] rounded-lg px-4 py-2
+    ml-2 focus:outline-none"
+          onMouseDown={() => {
+            if (navigator.vibrate) {
+              navigator.vibrate(100);
+            }
+          }}
+        >
+          <FaPlus />
+        </button>
       </div>
     </>
   );
