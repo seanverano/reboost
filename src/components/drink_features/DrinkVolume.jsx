7b63cc61269d9a6efcd71a062041d5782ae51699
Sean Dustin Verano <verano.seandustin@gmail.com>
2024-10-18 01:17:29 +0800

    Add reusable modal component for better structure

--- src/components/drink_features/DrinkVolume.jsx
+++ src/components/drink_features/DrinkVolume.jsx
@@ -1,19 +1,5 @@
 import React, { useState } from "react";
-
-const Modal = ({ show, onClose, children }) => {
-  if (!show) return null;
-
-  return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
-      <div className="bg-[#000000] rounded-lg p-6">
-        <button className="text-right text-gray-600" onClick={onClose}>
-          X
-        </button>
-        {children}
-      </div>
-    </div>
-  );
-};
+import Modal from "../Modal";
 
 const DrinkVolume = () => {
   const [showModal, setShowModal] = useState(false);
