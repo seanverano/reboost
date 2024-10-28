5db4653106743db625170ecd2edf4bcff963603b
Sean Dustin Verano <verano.seandustin@gmail.com>
2024-10-28 17:15:17 +0800

    Set up page routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import DrinkPage from "./pages/DrinkPage";
+import HomePage from "./pages/HomePage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-[#CEF3E6]">
       <div className="flex flex-col items-center justify-center w-[300px] h-[500px] bg-[#ffffff] shadow-lg rounded-lg">
-        <DrinkPage />
+        <Router>
+          <Routes>
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/hydration-tracker" element={<DrinkPage />} />
+          </Routes>
+        </Router>
       </div>
     </div>
   );
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,8 @@
+import { useNavigate } from "react-router-dom";
+
+const HomePage = () => {
+  return;
+  <></>;
+};
+
+export default HomePage;
