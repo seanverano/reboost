7c8744dcc57afd3e3ed297fb17515d4f07f05bfa
Sean Dustin Verano <verano.seandustin@gmail.com>
2024-10-29 22:18:56 +0800

    Change app name to Reboost

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# daily-reboot-chrome
+#Reboost
 
-"Daily Reboot" is a Google Chrome extension that keeps you active and refreshed with quick reminders to stand, stretch, and hydrate. Boost your well-being and stay on track effortlessly!
+"Reboost" is a Google Chrome extension that keeps you active and refreshed with quick reminders to stand, stretch, and hydrate. Boost your well-being and stay on track effortlessly!
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,7 +2,14 @@ import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
   return;
-  <></>;
+  <>
+    <h1 className="text-center font-bold text-[lg] leading-tight tracking-[-0.015em] px-4 mb-1">
+      Reboot
+    </h1>
+    <p className="flex justify-center px-2 text-[#4f8296] text-xs font-normal leading-normal mb-5">
+      Reboot your day with a wellness boost, one healthy habit at a time.
+    </p>
+  </>;
 };
 
 export default HomePage;
