6348e49ba72e933e0dbd75bcf7443d83940e8b41
Sean Dustin Verano <verano.seandustin@gmail.com>
2024-10-29 22:24:59 +0800

    Add useNavigate to home button in DrinkPage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,20 @@
 import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
-  return;
-  <>
-    <h1 className="text-center font-bold text-[lg] leading-tight tracking-[-0.015em] px-4 mb-1">
-      Reboot
-    </h1>
-    <p className="flex justify-center px-2 text-[#4f8296] text-xs font-normal leading-normal mb-5">
-      Reboot your day with a wellness boost, one healthy habit at a time.
-    </p>
-  </>;
+  const navigate = useNavigate();
+  const goDrink = () => navigate("/hydration-tracker");
+
+  return (
+    <>
+      <h1 className="text-center font-bold text-[lg] leading-tight tracking-[-0.015em] px-4 mb-1">
+        Reboot
+      </h1>
+      <p className="flex justify-center px-2 text-[#4f8296] text-xs font-normal leading-normal mb-5">
+        Reboot your day with a vibrant boost, one healthy habit at a time.
+      </p>
+      <button onClick={goDrink}>Hydration Tracker</button>
+    </>
+  );
 };
 
 export default HomePage;
