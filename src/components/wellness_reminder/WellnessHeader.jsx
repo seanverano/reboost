7e6b3fbc2b5ce96e40b75b4c60dc4e56128b2c1f
Sean Dustin Verano <verano.seandustin@gmail.com>
2024-11-01 07:04:18 +0800

    Add WellnessPage's Header component and create branch: feature/wellness-reminder

---
+++ src/components/wellness_reminder/WellnessHeader.jsx
@@ -0,0 +1,28 @@
+import { FiArrowLeft } from "react-icons/fi";
+import { GoGear } from "react-icons/go";
+import { useNavigate } from "react-router-dom";
+
+const WellnessHeader = () => {
+  const navigate = useNavigate();
+  const goMenu = () => navigate("/menu");
+
+  return (
+    <div className="flex items-center mt-3 p-1 pb-2 justify-between">
+      <button
+        onClick={goMenu}
+        className="flex cursor-pointer items-center justify-center rounded-full h-4 bg-transparent text-[#000000] gap-2 font-bold leading-normal tracking-[0.015em] p-0"
+      >
+        <div className="flex items-center gap-2 text-[#1CABE3] ml-3 hover:text-[#000000] text-lg">
+          <FiArrowLeft />
+        </div>
+      </button>
+      <button className="flex cursor-pointer items-center justify-center rounded-full h-4 bg-transparent text-[#000000] gap-2 font-bold leading-normal tracking-[0.015em] p-0">
+        <div className="flex items-center gap-2 text-[#1CABE3] mr-3 hover:text-[#000000] text-lg">
+          <GoGear />
+        </div>
+      </button>
+    </div>
+  );
+};
+
+export default WellnessHeader;
--- src/pages/WellnessPage.jsx
+++ src/pages/WellnessPage.jsx
@@ -1,7 +1,9 @@
+import WellnessHeader from "../components/wellness_reminder/WellnessHeader";
+
 const WellnessPage = () => {
   return (
     <div className="bg-[#F8FBFB] relative w-[300px] h-[500px] rounded-lg overflow-hidden font-manrope">
-      test
+      <WellnessHeader />
     </div>
   );
 };
